7551520dfa5e31c24b3d635ec615f67aeefb12d6
Iheanyi Oziegbe <neoconstel@gmail.com>
2022-12-30 12:33:17 +0100

    api: user membership (not logged in)

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('post/<pk>/', views.PostDetail.as_view(), name='post_detail'),
-    path('post/all/', views.PostList.as_view(), name='all_posts')
+    path('post/all/', views.PostList.as_view(), name='all_posts'),
+    path('membership/<pk>/', views.UserMembership.as_view(), name='membership'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 
 # models
-from .serializers import Post
+from .serializers import Post, User
 
 # serializers
-from .serializers import PostSerializer
+from .serializers import PostSerializer, UserSerializer
 
 # response / status
 from rest_framework.response import Response
@@ -29,3 +29,19 @@ class PostDetail(APIView):
         post = Post.objects.get(pk=pk)
         serializer = PostSerializer(post)
         return Response(serializer.data)
+
+
+class UserMembership(APIView):          
+
+     def get(self, request, pk=None):
+
+        # specific user
+        if pk:
+            user = User.objects.get(pk=pk)
+            serializer = UserSerializer(user)
+            return Response({'membership':serializer.data['membership']})
+        
+        # current logged-in user
+        else:
+            pass
+               
