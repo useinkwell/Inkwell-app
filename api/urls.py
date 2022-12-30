8e2b7e9bd7833fa6a514a227718670b85e3323b1
Iheanyi Oziegbe <neoconstel@gmail.com>
2022-12-30 09:22:09 +0100

    api: post_detail

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('post/<pk>/', views.PostDetail.as_view(), name='post_detail'),
     path('post/all/', views.PostList.as_view(), name='all_posts')
 ]
