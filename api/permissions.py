4fbf6e9dde473690200013d90c8d39a509d68850
Iheanyi Oziegbe <neoconstel@gmail.com>
2023-02-20 13:11:16 +0100

    add user-level permissions to API endpoints

---
+++ api/permissions.py
@@ -0,0 +1,31 @@
+from rest_framework import permissions
+
+IsAdmin = permissions.IsAdminUser
+IsAuthenticated = permissions.IsAuthenticated
+IsAuthenticatedElseReadOnly = permissions.IsAuthenticatedOrReadOnly
+
+
+class IsAdminElseReadOnly(permissions.IsAdminUser):
+    '''The request allows full access if user is admin, else it only allows
+    read-only access'''
+
+    def has_permission(self, request, view):
+        # if this is a GET, OPTIONS, or HEAD request
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        admin_permission = request.user and request.user.is_staff
+        return admin_permission
+
+
+class IsPostAuthorElseReadOnly(permissions.BasePermission):
+    '''The request allows full access if user is the PostAuthor, else it only
+    allows read-only access'''
+
+    def has_object_permission(self, request, view, obj):
+        # if this is a GET, OPTIONS, or HEAD request
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        user_permission = request.user and obj.user == request.user
+        return user_permission or request.user.is_staff
--- core/settings.py
+++ core/settings.py
@@ -133,6 +133,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'user.User'
 
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+		'rest_framework.permissions.IsAuthenticated',
+	    ],
+
     'DEFAULT_AUTHENTICATION_CLASSES': 
     (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
