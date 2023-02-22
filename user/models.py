7807b675b754e72a876fb40f80d6b4c5b17f35ad
Iheanyi Oziegbe <neoconstel@gmail.com>
2023-02-22 22:52:21 +0100

    Post model: add category and hashtags

--- user/models.py
+++ user/models.py
@@ -64,9 +64,11 @@ class User(AbstractBaseUser, PermissionsMixin):
 
 class Post(models.Model):
     title =  models.CharField(max_length=40, null=False)
+    category = models.CharField(max_length=40, null=True, blank=True)
     date_posted = models.DateTimeField(default=timezone.now)
     content = models.TextField(null=False)
     content_img = models.ImageField(upload_to="post_pics", null=False, default='post_default.ico')
+    hashtags = models.CharField(max_length=200, null=True, blank=True)
     user = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
 
     def __str__(self):
