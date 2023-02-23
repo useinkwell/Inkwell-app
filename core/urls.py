3d87dc965d5209a156625a6c60c9a20b7e8e6728
Iheanyi Oziegbe <neoconstel@gmail.com>
2023-02-23 22:36:01 +0100

    app structure: social_platform api modules grouped into 'api' directory

--- core/urls.py
+++ core/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('social_platform.urls'))
+    path('api/', include('social_platform.api.urls'))
 ]
