9589ea1d2626f8e3e558af54e1a9ecd9f4f1a96b
Iheanyi Oziegbe <neoconstel@gmail.com>
2023-02-23 23:11:17 +0100

    move Post and Comment models from User app to social_platform app

--- social_platform/models.py
+++ social_platform/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+
+class Post(models.Model):
+    title =  models.CharField(max_length=40, null=False)
+    category = models.CharField(max_length=40, null=True, blank=True)
+    date_posted = models.DateTimeField(default=timezone.now)
+    content = models.TextField(null=False)
+    content_img = models.ImageField(upload_to="post_pics", null=False, default='post_default.ico')
+    hashtags = models.CharField(max_length=200, null=True, blank=True)
+    user = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, null=False, on_delete=models.CASCADE)
+    body = models.TextField(null=False)
+    date_posted = models.DateTimeField(null=False, default=timezone.now)
+
+    def __str__(self):
+        return f"Comment('{self.user.user_name}', '{self.body}')"
+        
