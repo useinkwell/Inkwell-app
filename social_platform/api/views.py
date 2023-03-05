06e225a9c89b6aee2d275169abb6cf7e0f184897
Iheanyi Oziegbe <neoconstel@gmail.com>
2023-03-05 22:14:56 +0100

    Create Comment: parent_comment(if any) must belong to the post

--- social_platform/api/views.py
+++ social_platform/api/views.py
@@ -343,18 +343,28 @@ class CommentList(mixins.ListModelMixin, mixins.CreateModelMixin,
         return self.list(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
+
+        class CommentNotForPost(Exception):
+            pass
+
         try:
             post_id = self.kwargs['pk']
             post = Post.objects.get(pk=post_id)        
             parent_comment_id = self.request.GET.get('parent_comment')
             if parent_comment_id:
                 parent_comment = Comment.objects.get(id=parent_comment_id)
+                if parent_comment.post.pk != post.pk:
+                    raise CommentNotForPost
             else:
                 parent_comment = None
             content = self.request.GET.get('content')
         except Comment.DoesNotExist:
-            return Response({"error": "invalid parent comment id"}, 
+            return Response({"error": "invalid parent_comment id"}, 
                                             status=status.HTTP_404_NOT_FOUND)
+        except CommentNotForPost:
+            return Response(
+                {"error": f"post (id={post_id}) and parent_comment (post_id={parent_comment.post.pk}) do not match!"},
+                                            status=status.HTTP_400_BAD_REQUEST)
         
         new_comment = Comment.objects.create(
             user=self.request.user,
