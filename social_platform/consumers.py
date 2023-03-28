df3748247fec95f262a8ffe11df4162979cba2fc
Iheanyi Oziegbe <neoconstel@gmail.com>
2023-03-28 13:34:06 +0100

    notification: notify followers of new post via websocket

--- social_platform/consumers.py
+++ social_platform/consumers.py
@@ -3,11 +3,13 @@ from asgiref.sync import sync_to_async, async_to_sync
 import json
 
 from user.models import User
+from social_platform.models import Notification
 
 
 class ClientConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         user_name = self.scope['url_route']['kwargs']['user_name']
+
         print(f'socket: {user_name} connected')
 
         # store channel_name in session to be accessible from views
@@ -23,7 +25,15 @@ class ClientConsumer(AsyncWebsocketConsumer):
         await self.accept()
 
     
+    async def receive(self, text_data=None, bytes_data=None):
+        data = json.loads(text_data)
+        message = data['message']
+        print(f'\n\nFROM CLIENT: {message}\n\n')
+
+    
     async def client_sender(self, event):
+        '''just for testing client communication'''
+
         print(f'\n\nCLIENT SENDER IS RUNNING\n\n')
         message = event['message']
 
@@ -34,10 +44,40 @@ class ClientConsumer(AsyncWebsocketConsumer):
         ))
 
 
-    async def receive(self, text_data=None, bytes_data=None):
-        data = json.loads(text_data)
-        message = data['message']
-        print(f'\n\nFROM CLIENT: {message}\n\n')
+    async def notification(self, event):
+        print(f'\nNOTIFICATION IS RUNNING\n\n')
+        message = event['message']
+        action = event['action']
+        action_id = event['action_id']
+        by = event['by']
+
+        # create notification instance for all concerned users
+        sender = await sync_to_async(User.objects.get)(user_name=by)
+
+        @sync_to_async
+        def create_post_notification_instances():
+            followers_object = sender.followers.all()
+            followers = list(map(lambda follower_instance: \
+            follower_instance.follower, followers_object))
+            notifications = [Notification(
+                    message=message,
+                    user=follower
+                    ) for follower in followers]
+            Notification.objects.bulk_create(notifications)
+
+        if action == 'post':
+            await create_post_notification_instances()
+        
+
+        # send notification to client via websocket
+        await self.send(text_data=json.dumps(
+            {
+                'message': message,
+                'action': action,
+                'action_id': action_id,
+                'by': by
+            }
+        ))    
 
     
     @sync_to_async
