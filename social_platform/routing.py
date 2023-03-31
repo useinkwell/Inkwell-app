b01376758d39574a00c8977327bc3f2554643928
Iheanyi Oziegbe <neoconstel@gmail.com>
2023-03-31 11:11:14 +0100

    implement authenticated websocket connection via jwt access token

--- social_platform/routing.py
+++ social_platform/routing.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('ws/<str:user_name>/', consumers.ClientConsumer.as_asgi())
+    path('ws/<str:access_token>/', consumers.ClientConsumer.as_asgi())
 ]
