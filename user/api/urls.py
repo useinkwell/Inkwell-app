cc132cbf9bd639e3a3757d4e6cc6fcfe13d618bf
Iheanyi Oziegbe <neoconstel@gmail.com>
2023-04-16 23:10:36 +0100

    feature: forgot password/password reset

--- user/api/urls.py
+++ user/api/urls.py
@@ -12,5 +12,7 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 	path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', views.Register.as_view(), name="register"),
-    path('verify/', views.VerifyEmail.as_view(), name="verify_email") 
+    path('verify/', views.VerifyEmail.as_view(), name="verify_email"),
+    path('forgot_password/', views.ForgotPassword.as_view(), name="forgot_password"),
+    path('reset_password/', views.ResetPassword.as_view(), name="reset_password")
 ]
--- user/api/views.py
+++ user/api/views.py
@@ -80,6 +80,8 @@ class Register(APIView):
 
 
 class VerifyEmail(APIView):
+    '''when a user clicks the verification link in the email, this is the
+    view that handles the verification and subsequent redirection.'''
 
     # exempt this view from using authentication/permissions
     permission_classes = []
@@ -101,6 +103,82 @@ class VerifyEmail(APIView):
             return Response({user.user_name: "Verified"}, 
                             status=status.HTTP_202_ACCEPTED)
         return Response(status=status.HTTP_406_NOT_ACCEPTABLE)
+
+    
+class ForgotPassword(APIView):
+    '''this view collects the user email and sends an email to it containing
+    the url for password reset'''
+
+    # exempt this view from using authentication/permissions
+    permission_classes = []
+    authentication_classes = []
+
+    def post(self, request):
+
+        user_email = self.request.POST.get('email')
+        user = User.objects.filter(email=user_email).first()
+        if user:
+        
+            # generate fresh jwt access token for user
+            access_token = get_jwt_access_tokens_for_user(user)['access_token']
+
+            # encrypt access token for sending in password reset URL
+            encrypted_access_token = \
+                encryption_handler.encrypt(access_token.encode())
+
+            # send password reset email with user password reset URL
+            send_email(
+                'Inkwell Password Reset',
+                f'''
+                Click this link to reset your Inkwell password:
+                http://127.0.0.1:8000/auth/reset_password/?x_access_token={encrypted_access_token.decode()}/
+                ''',
+                'no-reply@inkwellteam.com',
+                user_email
+            )
+            print(f'\n\nPASSWORD RESET TOKEN:{encrypted_access_token.decode()}\n\n')
+            return Response(status=status.HTTP_200_OK)
+            
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+class ResetPassword(APIView):
+    '''when a user clicks the password reset link in the email, this is the
+    view that handles the password reset and subsequent redirection.'''
+
+    # exempt this view from using authentication/permissions
+    permission_classes = []
+    authentication_classes = []
+
+    
+    def get_user(self):
+        # the encrypted access token is received here and decoded
+        encrypted_access_token = \
+            self.request.GET.get('x_access_token').replace('/','').encode()
+        access_token = \
+            encryption_handler.decrypt(encrypted_access_token).decode()
+
+        token_user_data = verify_token(access_token)
+        if token_user_data:
+            user = User.objects.get(id=token_user_data['user_id'])
+            return user
+        return None
+
+
+    def post(self, request):
+        password = self.request.POST.get('password')
+        password2 = self.request.POST.get('password2')
+        user = self.get_user()
+
+        if user and password == password2:
+            user.set_password(password)
+            user.save()
+            
+            return Response(status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+
 
 
 def get_jwt_access_tokens_for_user(user_instance):
